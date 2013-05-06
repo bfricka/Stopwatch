06ad4d06991dfe297caa11b3d6a6953623c721cb
Brian Frichette <brian.frichette@gmail.com>
2013-05-05 23:01:31 -0700

    Unit tests

--- test/spec/timer.spec.js
+++ test/spec/timer.spec.js
@@ -22,11 +22,87 @@ describe('Timer defaults', function(){
 
   it('should fire a start event when started', function(){
     var startFired = false;
-    timer.on('start', function(){
-      startFired = true;
-    });
+    timer.on('start', function(){ startFired = true; });
     timer.start();
 
     expect(startFired).toBe(true);
+  });
+
+  it('should fire a stop event when stopped', function(){
+    var stopFired = false;
+    timer.on('stop', function(){ stopFired = true; });
+    timer.start();
+    timer.stop();
+
+    expect(stopFired).toBe(true);
+  });
+
+  it('should fire a pause event', function(){
+    var pauseFired = false;
+    timer.on('pause', function(){ pauseFired = true; });
+    timer.start();
+    timer.pause();
+
+    expect(pauseFired).toBe(true);
+  });
+
+  it('should fire a restart event but not a start event', function(){
+    var restartFired = false
+    , startFired = false;
+
+    timer.on('restart', function(){ restartFired = true; });
+    timer.on('start', function(){ startFired = true; });
+
+    timer.restart();
+
+    expect(restartFired).toBe(true);
+    expect(startFired).toBe(false);
+  });
+});
+
+describe('Timer parseTime', function(){
+  // Use a single instance and just place with parseTime
+  var timer = new Timer();
+
+  it('should parse seconds correctly', function(){
+    timer.parseTime('5s');
+    expect(timer.getMaxTime()).toEqual(5);
+  });
+
+  it('should parse minutes correctly', function(){
+    timer.parseTime('2m');
+    expect(timer.getMaxTime()).toEqual(120);
+  });
+
+  it('should parse hours correctly', function(){
+    timer.parseTime('1h');
+    expect(timer.getMaxTime()).toEqual(3600);
+  });
+
+  it('should also parse floats correctly', function(){
+    timer.parseTime('0.5h');
+    expect(timer.getMaxTime()).toEqual(1800);
+  });
+});
+
+describe('Timer behavior', function(){
+  var timerCallback;
+
+  beforeEach(function(){
+    timerCallback = jasmine.createSpy('timerCallback');
+    jasmine.Clock.useMock();
+  });
+
+  it('should stop at maxTime and fire callback', function(){
+    var timer = new Timer('5s');
+
+    timer.on('stop', function(){ timerCallback(); });
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    timer.start();
+    // Tick 5 seconds
+    jasmine.Clock.tick(5000);
+
+    expect(timerCallback).toHaveBeenCalled();
   });
 });
